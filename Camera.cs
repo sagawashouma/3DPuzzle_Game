e43577ab7ac44f8fd7c15428744b2dfd2f2cc5a4
sagawashouma <73643223+sagawashouma@users.noreply.github.com>
2021-06-11 09:40:45 +0900

    Revert "Revert "カメラ操作のScriptです。""
    This reverts commit 4cb98197276e6e51ceba3d85d17a4691abdf88f5.

---
+++ Camera.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject MainCamera;                   //メインカメラ
+    public GameObject Target;                       //ターゲット
+
+
+
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Debug.Log("LeftClick" + mousePosition);
+
+            if(mousePosition.x < 700)
+            {
+                Debug.Log("LEFT");
+                transform.LookAt(Target.transform.position);
+                transform.RotateAround(Target.transform.position, new Vector3(0, 1, 0), 0.5f);
+            }
+            else if(mousePosition.x >= 700)
+            {
+                Debug.Log("RIGHT");
+                transform.LookAt(Target.transform.position);
+                transform.RotateAround(Target.transform.position, new Vector3(0, 1, 0), -0.5f);
+            }
+            
+        }
+    }
+}
